795d4cb77628e17d4241dd8a2d93a76c1b4efc45
Carr <lcarr@usgs.gov>
2018-07-10 13:56:28 -0500

    move sites subsetting to custom function

--- 1_fetch/src/fetch_sites.R
+++ 1_fetch/src/fetch_sites.R
@@ -32,27 +32,3 @@ fetch_sites_from_sf <- function(ind_file, sf_object, dates, stream_params) {
   gd_put(ind_file, data_file)
 
 }
-
-#' Subset the site list to ones relevant for the storm
-#'
-#' @param ind_file character file name where the output should be saved
-#' @param all_sites data.frame of sites pulled for the bbox
-subset_sites <- function(ind_file, all_sites) {
-
-  all_sites <- readRDS(scipiper::sc_retrieve(all_sites))
-
-  # subset the sites from the wide net cast to ones relevant to the storm
-  # subset criteria TBD
-  sites_info <- dataRetrieval::readNWISsite(siteNumbers = all_sites)
-
-  #############********* temporarily grab only a few sites *********#############
-  set.seed(10)
-  sites_info <- dplyr::sample_n(sites_info, 50)
-  #############*********
-
-  # write the data file and the indicator file
-  data_file <- as_data_file(ind_file)
-  feather::write_feather(sites_info, data_file)
-  gd_put(ind_file, data_file)
-
-}
