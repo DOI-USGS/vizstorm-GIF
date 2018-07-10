8f38cf67ef8e1be41c34795ba0933430ed7cab38
Carr <lcarr@usgs.gov>
2018-07-10 14:42:00 -0500

    custom filtering for large drainage areas

--- 2_process/src/filter_sites_custom.R
+++ 2_process/src/filter_sites_custom.R
@@ -3,22 +3,21 @@
 #'
 #' @param ind_file character file name where the output should be saved
 #' @param sites data.frame of sites with automatic filtering applied already
-filter_sites_custom <- function(ind_file, sites) {
+filter_sites_custom <- function(ind_file, sites_file) {
 
-  sites <- readRDS(scipiper::sc_retrieve(sites))
+  sites <- readRDS(scipiper::sc_retrieve(sites_file))
 
   # subset the sites from the wide net cast to ones relevant to the storm
   # subset criteria TBD
   sites_info <- dataRetrieval::readNWISsite(siteNumbers = sites)
 
-  #############********* temporarily grab only a few sites *********#############
-  set.seed(10)
-  sites_info <- dplyr::sample_n(sites_info, 50)
-  #############*********
+  sites_info_subset <- sites_info %>%
+    # only include sites with large drainage areas
+    filter(drain_area_va > quantile(drain_area_va, 0.75, na.rm=TRUE))
 
   # write the data file and the indicator file
   data_file <- as_data_file(ind_file)
-  feather::write_feather(sites_info, data_file)
+  feather::write_feather(sites_info_subset, data_file)
   gd_put(ind_file, data_file)
 
 }
