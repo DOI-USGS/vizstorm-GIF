c429ba6029377ec2602af40270da24233f2647f0
Alison Appling <aappling@usgs.gov>
2018-07-10 08:48:43 -0400

    interpolate storm points

--- 2_process/src/interpolate_storm_points.R
+++ 2_process/src/interpolate_storm_points.R
@@ -1,29 +1,23 @@
+#' create an sf POINTS collection with a point along the storm track for each
+#' timestep. interpolates from coarse_points
 interpolate_storm_points <- function(ind_file, timesteps_ind, coarse_points_ind) {
-  # -- create a SpatialPointsDataFrame with a point for each timestep --
+
+  # read in the coarse points storm track
+  coarse_points <- readRDS(sc_retrieve(coarse_points_ind))
 
   # get the vector of timepoints we want to include
-  times <- as.POSIXct(strptime(depends[['timesteps']]$times, format='%b %d %I:%M %p', tz="America/Puerto_Rico"))
+  timesteps <- readRDS(sc_retrieve(timesteps_ind))
 
-  # interpolate latitudes and longitudes to the timestamps we actually want
+  # interpolate the already-projected points to the timestamps we actually want
   coords_interp <- data_frame(
-    lat_interp = approx(points_simpler$DateTime, points_simpler$LAT, xout = times)$y,
-    lon_interp = approx(points_simpler$DateTime, points_simpler$LON, xout = times)$y) %>%
+    DateTime = timesteps,
+    X = approx(coarse_points$DateTime, sf::st_coordinates(coarse_points)[,'X'], xout = timesteps)$y,
+    Y = approx(coarse_points$DateTime, sf::st_coordinates(coarse_points)[,'Y'], xout = timesteps)$y) %>%
     filter(., complete.cases(.))
-  pts <- cbind(lon_interp[!is.na(lon_interp)], lat_interp[!is.na(lon_interp)])
-  location <- SpatialPoints(pts, proj4string=CRS("+proj=longlat +datum=WGS84"))
+  coords_interp_sf <- sf::st_as_sf(coords_interp, coords=c('X','Y'))
 
-  # construct a SpatialPointsDataFrame with the above locations. No timestamps,
-  # just locations that precisely correspond to the times from the `timesteps`
-  # item
-  data.out <- data.frame(
-    id = paste0('storm-', seq_len(length(location))),
-    class = "storm-dot",
-    r = "12",
-    stringsAsFactors = FALSE)
-  row.names(data.out) <- row.names(location)
-  sp.data.frame <- as(object = location, Class = paste0(class(location), "DataFrame"))
-  sp.data.frame@data <- data.out
-  row.names(sp.data.frame) <- row.names(data.out) # i think this line is unnecessary but harmless
-
-
+  # write the sf POINTS object to file
+  data_file <- as_data_file(ind_file)
+  saveRDS(coords_interp_sf, data_file)
+  gd_put(ind_file, data_file)
 }
