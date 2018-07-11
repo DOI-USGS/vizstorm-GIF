540b17ce93f625ac2bea04a78b6c7934dc26cf53
Carr <lcarr@usgs.gov>
2018-07-11 08:54:19 -0500

    pass around nwis site list as object instead of file+ind

--- 1_fetch/src/fetch_sites.R
+++ 1_fetch/src/fetch_sites.R
@@ -11,11 +11,10 @@ fetch_states_from_sf <- function(sf_object) {
 
 #' Download all NWIS stage sites within the bounding box of a supplied spatial object
 #'
-#' @param ind_file character file name where the output should be saved
 #' @param state_cds spatial object with IDs indicating the state name
 #' @param dates object from viz_config.yaml that specifies dates as string
 #' @param stream_params pcodes to use from NWIS
-fetch_sites_from_states <- function(ind_file, state_cds, dates, stream_params) {
+fetch_sites_from_states <- function(state_cds, dates, stream_params) {
 
   # Cast wide net for all NWIS sites with stage data that fall within that bbox
   sites_df <- data.frame()
@@ -23,15 +22,12 @@ fetch_sites_from_states <- function(ind_file, state_cds, dates, stream_params) {
     sites_df_cd <- dataRetrieval::whatNWISdata(stateCd = cd, parameterCd = stream_params$stage, service = "uv")
     sites_df <- rbind(sites_df, sites_df_cd)
   }
+
   sites_df <- dplyr::filter(sites_df, site_tp_cd == "ST")
   sites_df <- dplyr::filter(sites_df, end_date >= as.Date(dates$end))
-  sites <- sites_df$site_no
+  sites <- dplyr::select(sites_df, site_no)
 
-  # write the data file and the indicator file
-  data_file <- as_data_file(ind_file)
-  saveRDS(sites, data_file)
-  gd_put(ind_file, data_file)
-
+  return(sites)
 }
 
 #' Subset the site list to ones relevant for the storm
@@ -40,11 +36,9 @@ fetch_sites_from_states <- function(ind_file, state_cds, dates, stream_params) {
 #' @param all_sites data.frame of sites pulled for the bbox
 subset_sites <- function(ind_file, all_sites) {
 
-  all_sites <- readRDS(scipiper::sc_retrieve(all_sites))
-
   # subset the sites from the wide net cast to ones relevant to the storm
   # subset criteria TBD
-  sites_info <- dataRetrieval::readNWISsite(siteNumbers = all_sites)
+  sites_info <- dataRetrieval::readNWISsite(siteNumbers = all_sites$site_no)
 
   #############********* temporarily grab only a few sites *********#############
   set.seed(10)
