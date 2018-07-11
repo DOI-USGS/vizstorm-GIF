bcbb2658cb6b31ce0ca3f7dc10910392c509a16f
Carr <lcarr@usgs.gov>
2018-07-11 09:43:13 -0500

    hook up nws stage filtering to final storm sites

--- 2_process/src/filter_sites_custom.R
+++ 2_process/src/filter_sites_custom.R
@@ -2,22 +2,24 @@
 #' Subset the site list to ones relevant for the storm
 #'
 #' @param ind_file character file name where the output should be saved
-#' @param sites_file path for data.frame of sites with automatic filtering applied already
+#' @param sites_ind ind file for data.frame of sites with automatic filtering applied already
 filter_sites_custom <- function(ind_file, sites_file) {
 
-  sites <- readRDS(scipiper::sc_retrieve(sites_file))
+  sites_df <- readRDS(scipiper::sc_retrieve(sites_file))
 
   # subset the sites from the wide net cast to ones relevant to the storm
-  # subset criteria TBD
-  sites_info <- dataRetrieval::readNWISsite(siteNumbers = sites)
+  # subset criteria TBD for each storm
+  sites_info <- dataRetrieval::readNWISsite(siteNumbers = sites_df$site_no)
 
   sites_info_subset <- sites_info %>%
     # only include sites with large drainage areas
     filter(drain_area_va > quantile(drain_area_va, 0.75, na.rm=TRUE))
 
+  final_sites_df <- left_join(sites_info_subset, sites_df, by = "site_no")
+
   # write the data file and the indicator file
   data_file <- as_data_file(ind_file)
-  feather::write_feather(sites_info_subset, data_file)
+  feather::write_feather(final_sites_df, data_file)
   gd_put(ind_file, data_file)
 
 }
