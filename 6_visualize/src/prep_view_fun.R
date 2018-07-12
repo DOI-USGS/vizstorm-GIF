7695a95dcc7ccba7592f8279d89e2ef0f81b551e
Jordan S Read <jread@usgs.gov>
2018-07-12 15:28:16 -0500

    forgot to pull upstream

--- 6_visualize/src/prep_view_fun.R
+++ 6_visualize/src/prep_view_fun.R
@@ -1,7 +1,7 @@
 
 prep_view_fun <- function(view_polygon){
   plot_fun <- function(){
-    par(omi = c(0,0,0,0), mai = c(0,0,0,0), bg = '#B5D3E7')
+    par(omi = c(0,0,0,0), mai = c(0,0,0,0), bg = '#E1EDF5')
     plot(view_polygon, col = NA, border = NA, xaxs = 'i', yaxs = 'i')
   }
   return(plot_fun)
