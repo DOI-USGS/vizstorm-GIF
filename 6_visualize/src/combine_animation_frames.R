474e75138bac46ad700cfb50c138803aed933326
Alison Appling <aappling@usgs.gov>
2018-07-13 09:12:40 -0400

    bugfix in combine_animation_frames (was ignoring task_names)

--- 6_visualize/src/combine_animation_frames.R
+++ 6_visualize/src/combine_animation_frames.R
@@ -2,9 +2,9 @@ combine_animation_frames <- function(gif_file, animation_cfg, task_names=NULL) {
 
   # run imageMagick convert to build a gif
   if(is.null(task_names)) task_names <- '*'
-  png_files <- paste(sprintf('6_visualize/tmp/gif_frame_%s.png', task_names), collapse=',')
+  png_files <- paste(sprintf('6_visualize/tmp/gif_frame_%s.png', task_names), collapse=' ')
   magick_command <- sprintf('magick convert -delay %d -loop 0 %s %s',
-                            animation_cfg$frame_delay_cs, '6_visualize/tmp/gif_frame_*.png', gif_file)
+                            animation_cfg$frame_delay_cs, png_files, gif_file)
   system(magick_command)
 
   # simplify the gif with gifsicle - cuts size by about 2/3
