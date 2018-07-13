505135641d1cf90a37f898d3bf0f87d60dbbf474
Alison Appling <aappling@usgs.gov>
2018-07-13 17:26:00 -0400

    gage2spark might be working, needs real coords

--- 6_visualize/src/prep_gage2spark_fun.R
+++ 6_visualize/src/prep_gage2spark_fun.R
@@ -14,11 +14,14 @@ prep_gage2spark_fun <- function(n_timesteps, timestep) {
 
   # calculate additional variables. naming convention for d and t: lowercase =
   # position in space/time, uppercase = length/duration
+  r_revised <- pmin(r, x2-x1, y2-y1)
+  r_rise <- r_revised * sign(y2 - y1)
+  r_run <- r_revised * sign(x2 - x1)
   T_move <- t_end - t_start # length of the move in number of timesteps. probably the same for all sites
-  D_rise <- abs(y2 - r - y1) # length of the purely vertical segment of the travel path
+  D_rise <- y2 - r_rise - y1 # length of the purely vertical segment of the travel path
   D_round <- pi*r/2 # length of the segment of the travel path that rounds the corner from vertical to horizontal
-  D_run <- abs(x2 - r - x1) # length of the purely horizontal segment of the travel path
-  D <- D_rise + D_round + D_run # length of the full travel path
+  D_run <- x2 - r_run - x1 # length of the purely horizontal segment of the travel path
+  D <- abs(D_rise) + abs(D_round) + abs(D_run) # length of the full travel path
 
   # define phases by their first timestep (different for each site=row)
   phases <- data_frame(
@@ -31,12 +34,36 @@ prep_gage2spark_fun <- function(n_timesteps, timestep) {
     as.matrix()
 
   phase <- sapply(seq_len(n_sites), function(site) {
-    cut(timestep, breaks=phases[site,], labels=colnames(phases)[-6], right=FALSE)
-  })
-  phase_pos <- (timestep
+    breaks_all <- phases[site,]
+    empty_bins <- breaks_all[diff(breaks_all)==0]
+    breaks_nonempty <- breaks_all[setdiff(names(breaks_all), names(empty_bins))]
+    cut(timestep, breaks=breaks_nonempty, labels=setdiff(names(breaks_nonempty), 'end'), right=FALSE) %>%
+      as.character()
+  }) %>% ordered(levels=colnames(phases))
+  # calculate phase_pos, the fraction of the way each site is through its current phase
+  phase_start <- phases[cbind(seq_len(n_sites), phase)]
+  phase_end <- phases[cbind(seq_len(n_sites), as.numeric(phase)+1)]
+  phase_pos <- (timestep - phase_start)/(phase_end - phase_start)
+  theta <- phase_pos*pi/2 # angle as fraction of 90 degrees (pi/2 radians); calculated for all, applies only to round phase
 
-  plot_fun <- function(){
-    # empty placeholder for now
+  # calculate coordinates for this timestep for each site
+  too_many_coords <- list(
+    prelude = data_frame(x=x1, y=y1),
+    rise = data_frame(x=x1, y=y1 + phase_pos*D_rise),
+    round = data_frame(x=x1 + r_run*(1-cos(theta)), y=y2 - r_rise*(1 - sin(theta))),
+    run = data_frame(x=x1 + r_run + phase_pos*D_run, y=y2),
+    epilog = data_frame(x=x2, y=y2))
+  coords <- bind_rows(lapply(seq_len(n_sites), function(site) {
+    too_many_coords[[as.character(phase[site])]][site,]
+  }))
+
+  # make a sterile environment with nothing except what we need in the closure,
+  # then make the function
+  clean_env <- function(coords) {
+    function(){
+      plot(coords, add=TRUE)
+    }
   }
+  plot_fun <- clean_env(coords)
   return(plot_fun)
 }
