b013a50ac9a8b81567c42880f1fbf6edc229779c
Alison Appling <aappling@usgs.gov>
2018-07-19 17:34:37 -0400

    restore the sf-ness of normalized streamdata

--- 2_process/src/normalize_streamdata.R
+++ 2_process/src/normalize_streamdata.R
@@ -4,7 +4,7 @@ normalize_streamdata <- function(ind_file, raw_ind_file, sites_ind_file){
 
   streamdata <- readRDS(sc_retrieve(raw_ind_file)) # NWIS raw data
   storm_sites <- readRDS(sc_retrieve(sites_ind_file))
-  storm_data <- left_join(streamdata, storm_sites)
+  storm_data <- left_join(streamdata, storm_sites, by='site_no')
 
   # we assume "normalized" stage is between 0 and 1
   # if that changes, see 6_visualize/src/prep_spark_line_fun.R
@@ -16,8 +16,9 @@ normalize_streamdata <- function(ind_file, raw_ind_file, sites_ind_file){
     mutate(stage_normalized = (stage - min(stage, na.rm = T)) / (max(stage, na.rm = T) - min(stage, na.rm = T))) %>%
     mutate(flood_stage_normalized = (as.numeric(flood_stage) - min(stage, na.rm = T)) / (max(stage, na.rm = T) - min(stage, na.rm = T))) %>%
     ungroup()
+  norm_stream_sf <- sf::st_as_sf(norm_stream)
 
   data_file <- as_data_file(ind_file)
-  saveRDS(norm_stream, data_file)
+  saveRDS(norm_stream_sf, data_file)
   gd_put(ind_file, data_file)
 }
