5d596735c71a83655eadbd26f4e3d66df4ce405a
Alison Appling <aappling@usgs.gov>
2018-07-22 16:35:48 -0400

    use timesteps to choose start_date

--- 6_visualize/src/visualize_utils.R
+++ 6_visualize/src/visualize_utils.R
@@ -1,8 +1,7 @@
 
 fetch_read <- function(ind_file) readRDS(sc_retrieve(ind_file))
 
-format_start_date <- function(storm_data) {
-  storm_data <- arrange(storm_data, dateTime)
-  start_date <- storm_data$dateTime[1]
-  return(start_date)
+format_start_date <- function(timestep_ind) {
+  timesteps <- fetch_read(timestep_ind)
+  return(timesteps[1])
 }
