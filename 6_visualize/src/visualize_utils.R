0199e2105551005cf0a9f147e1dd4378a395a7d1
Alison Appling <aappling@usgs.gov>
2018-07-22 17:34:29 -0400

    fiddle with trial and final frame selection

--- 6_visualize/src/visualize_utils.R
+++ 6_visualize/src/visualize_utils.R
@@ -5,3 +5,9 @@ format_start_date <- function(timestep_ind) {
   timesteps <- fetch_read(timestep_ind)
   return(timesteps[1])
 }
+
+existing_png_tasks <- function(path='6_visualize/tmp', pattern='a_[[:digit:]]{8}_[[:digit:]]{2}') {
+  files <- dir(path, pattern=sprintf('%s.*\\.png', pattern))
+  tasks <- regmatches(files, regexpr(sprintf("(%s)", pattern), files))
+  return(tasks)
+}
