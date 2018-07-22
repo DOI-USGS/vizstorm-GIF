579a2e86e16d612a8c420d8b28af0aed1d43d1ec
Alison Appling <aappling@usgs.gov>
2018-07-22 17:41:30 -0400

    fetch enough streamdata to compensate for local vs UTC

--- 1_fetch/src/fetch_streamdata.R
+++ 1_fetch/src/fetch_streamdata.R
@@ -3,9 +3,13 @@ fetch_streamdata <- function(ind_file, sites_ind, dates, stream_params, gd_confi
   sites <- readRDS(scipiper::sc_retrieve(sites_ind))
 
   message('  starting download of NWIS data at ', Sys.time())
+  dates_with_buffer <- list(
+    start = (as.POSIXct(dates$start, tz='UTC') - as.difftime(1, units='days')) %>% format('%Y-%m-%d'),
+    end = (as.POSIXct(dates$end, tz='UTC') + as.difftime(1, units='days')) %>% format('%Y-%m-%d')
+  )
   data <- readNWISuv(
     siteNumbers=sites$site_no, parameterCd=stream_params$stage,
-    startDate=dates$start, endDate=dates$end)
+    startDate=dates_with_buffer$start, endDate=dates_with_buffer$end)
 
   data_file <- as_data_file(ind_file)
   saveRDS(data, data_file)
