b91e9812a72bbda32a2ee9826de65e0de3489c61
Alison Appling <aappling@usgs.gov>
2018-09-05 13:21:32 -0400

    make storm line color adjustable and gray

--- 6_visualize/src/prep_storm_line_fun.R
+++ 6_visualize/src/prep_storm_line_fun.R
@@ -1,11 +1,11 @@
 
-prep_storm_line_fun <- function(storm_line_ind_file){
+prep_storm_line_fun <- function(storm_line_ind_file, storm_line_cfg){
   storm_line_sf <- readRDS(sc_retrieve(storm_line_ind_file))
   plot_fun <- if(is.null(storm_line_sf)) {
     function() {} # storms are optional
   } else {
     function(){
-      plot(sf::st_geometry(storm_line_sf), add = TRUE, col = 'black', lwd = 2)
+      plot(sf::st_geometry(storm_line_sf), add = TRUE, col = storm_line_cfg$storm_line_col, lwd = 2)
     }
   }
   return(plot_fun)
