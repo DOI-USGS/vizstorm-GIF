eecdfc10566853e001a369a1f51c1b9b909143a5
David Blodgett <dblodgett@usgs.gov>
2018-09-13 06:02:42 -0500

    fixes #140 precip boundary in GDP data pull is now used to clip the stencil.

--- 1_fetch/src/map_utils.R
+++ 1_fetch/src/map_utils.R
@@ -6,9 +6,10 @@
 #'
 #' @return an sf polygon that based on coordinates of `bbox`
 bbox_to_polygon <- function(bbox, bbox_crs = "+init=epsg:4326", return_crs = NULL) {
-  bbox_poly <- sf::st_sfc(sf::st_polygon(list(
-    matrix(bbox[c(1, 2, 1, 4, 3, 4, 3, 2, 1, 2)], ncol = 2, byrow = TRUE)),
-    dim = "XY"), crs = bbox_crs)
+  names(bbox) <- c("xmin","ymin","xmax","ymax")
+  class(bbox) <- "bbox"
+  bbox_poly <- sf::st_as_sfc(bbox)
+  st_crs(bbox_poly) <- st_crs(bbox_crs)
   if (!is.null(return_crs)) {
     sf::st_transform(bbox_poly, return_crs)
   }
@@ -29,7 +30,7 @@ as_view_polygon <- function(view_config) {
 
   # the projected polygon specified by the user
   bbox_projected <- bbox_to_polygon(bbox = view_config$bbox, return_crs = view_config$projection)
-  poly_bbox <- sf::st_bbox(bbox_projected)
+  poly_bbox <- as.numeric(sf::st_bbox(bbox_projected))
   aspect <- view_config$width / view_config$height
   bbox_aspect <- diff(poly_bbox[c(1,3)]) / diff(poly_bbox[c(2,4)])
   if (bbox_aspect > aspect){ # flatter than it should be
@@ -39,8 +40,11 @@ as_view_polygon <- function(view_config) {
     poly_bbox[4] <- poly_bbox[4] + y_buffer / 2
 
   } else { # taller than it should be
-    new_x_diff <- diff(poly_bbox[c(2,4)]) * aspect
+    # new x dimension
+    new_x_diff <- diff(poly_bbox[c(2,4)]) * aspect 
+    # new x dimension - existing y dimension
     x_buffer <- new_x_diff - diff(poly_bbox[c(1,3)])
+    # subtract half the buffer from min x, add half to max x
     poly_bbox[1] <- poly_bbox[1] - x_buffer / 2
     poly_bbox[3] <- poly_bbox[3] + x_buffer / 2
   }
