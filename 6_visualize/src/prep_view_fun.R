529b9ad0c0b4519e56984ff34c6ad7df19b9aa9b
Jordan S Read <jread@usgs.gov>
2018-09-14 11:09:24 -0500

    rebuild w/ dates and styles

--- 6_visualize/src/prep_view_fun.R
+++ 6_visualize/src/prep_view_fun.R
@@ -8,7 +8,8 @@ prep_view_fun <- function(view_polygon, view_cfg){
 }
 
 prep_bbox_fun <- function(view_config){
-  bbox <- bbox_to_polygon(bbox = view_config$bbox, return_crs = view_config$projection)
+  
+  bbox <- bbox_to_polygon(bbox = unlist(view_config$bbox), return_crs = view_config$projection)
 
   plot_fun <- function(){
     plot(bbox, add = TRUE, border = 'green', col = NA, lty = 3, lwd = 3)
