76d9ae89bac0424728406f247d76b921e45004be
Jordan S Read <jread@usgs.gov>
2018-09-15 13:41:25 -0500

    full insta build

--- 6_visualize/src/create_gif_tasks.R
+++ 6_visualize/src/create_gif_tasks.R
@@ -3,22 +3,22 @@
 # over time
 
 create_intro_gif_tasks <- function(intro_config, folders, storm_track_cfg, storm_start_date){
-
+  
   # set up a series of animation frame timesteps (not related to actual time,
   # just describe time within the explanatory animation)
   timestep <- seq_len(intro_config$n_frames)
   has_storm_track <- !is.null(storm_track_cfg$storm_code)
-
+  
   # aspect/resolution configuration placeholder. see same code in
   # create_storm_gif_tasks - we should extract this into shared location once we
   # get multiple configurations really going
   cfgs <- c('a')
-
+  
   tasks <- tidyr::crossing(timestep, cfgs) %>%
     unite(task_name, cfgs, timestep, sep = '_', remove = F) %>%
     mutate(date_hour = sprintf('01_%03d', timestep),
            task_name = sprintf("%s_%s", cfgs, date_hour))
-
+  
   gage2spark_frame <- scipiper::create_task_step(
     step_name = 'gage2spark_frame',
     target_name = function(task_name, step_name, ...){
@@ -40,7 +40,7 @@ create_intro_gif_tasks <- function(intro_config, folders, storm_track_cfg, storm
       )
     }
   )
-
+  
   legend_frame <- scipiper::create_task_step(
     step_name = 'legend_frame',
     target_name = function(task_name, step_name, ...){
@@ -61,7 +61,7 @@ create_intro_gif_tasks <- function(intro_config, folders, storm_track_cfg, storm
       )
     }
   )
-
+  
   gif_frame <- scipiper::create_task_step(
     step_name = 'gif_frame',
     target_name = function(task_name, step_name, ...){
@@ -86,7 +86,7 @@ create_intro_gif_tasks <- function(intro_config, folders, storm_track_cfg, storm
       )
     }
   )
-
+  
   gif_task_plan <- scipiper::create_task_plan(
     task_names=tasks$task_name,
     task_steps=list(gage2spark_frame, legend_frame, gif_frame),
@@ -96,18 +96,18 @@ create_intro_gif_tasks <- function(intro_config, folders, storm_track_cfg, storm
 }
 
 create_storm_gif_tasks <- function(timestep_ind, storm_track_cfg, folders){
-
+  
   timestep <- readRDS(sc_retrieve(timestep_ind))
   has_storm_track <- !is.null(storm_track_cfg$storm_code)
-
+  
   cfgs <- c('a') # for now, just use one config, since > 1 results in duplication of input files
   #,'b') # dummy placement for different configurations; will eventually be configurations that hold information about size, aspect, ect...
-
+  
   tasks <- tidyr::crossing(timestep, cfgs) %>%
     unite(task_name, cfgs, timestep, sep = '_', remove = F) %>%
     mutate(date_hour = strftime(timestep, format = '%Y%m%d_%H', tz = 'UTC'),
            task_name = sprintf("%s_%s", cfgs, date_hour))
-
+  
   sites_frame <- scipiper::create_task_step(
     step_name = 'sites_frame',
     target_name = function(task_name, step_name, ...){
@@ -124,7 +124,7 @@ create_storm_gif_tasks <- function(timestep_ind, storm_track_cfg, folders){
       )
     }
   )
-
+  
   if(has_storm_track) {
     storm_point_frame <- scipiper::create_task_step(
       step_name = 'storm_point_frame',
@@ -143,7 +143,7 @@ create_storm_gif_tasks <- function(timestep_ind, storm_track_cfg, folders){
       }
     )
   }
-
+  
   precip_frame <- scipiper::create_task_step( # not sure why this is called "frame".
     step_name = 'precip_frame',
     target_name = function(task_name, step_name, ...){
@@ -155,7 +155,7 @@ create_storm_gif_tasks <- function(timestep_ind, storm_track_cfg, folders){
       sprintf("prep_precip_fun(precip_rasters, precip_bins, I('%s'))", format(cur_task$timestep, "%Y-%m-%d %H:%M:%S"))
     }
   )
-
+  
   spark_frame <- scipiper::create_task_step(
     step_name = 'spark_frame',
     target_name = function(task_name, step_name, ...){
@@ -171,10 +171,11 @@ create_storm_gif_tasks <- function(timestep_ind, storm_track_cfg, folders){
         "timestep_ind = '2_process/out/timesteps.rds.ind',",
         "sparkline_placement,",
         "gage_color_config,",
-        "I('%s'))"=cur_task$timestep)
+        "I('%s'),"=cur_task$timestep,
+        "legend_text_cfg = legend_text_cfg)")
     }
   )
-
+  
   datetime_frame <- scipiper::create_task_step(
     step_name = 'datetime_frame',
     target_name = function(task_name, step_name, ...){
@@ -186,7 +187,7 @@ create_storm_gif_tasks <- function(timestep_ind, storm_track_cfg, folders){
       sprintf("prep_datetime_fun(I('%s'), datetime_placement, date_display_tz)", format(cur_task$timestep, "%Y-%m-%d %H:%M:%S"))
     }
   )
-
+  
   legend_frame <- scipiper::create_task_step(
     step_name = 'legend_frame',
     target_name = function(task_name, step_name, ...){
@@ -207,7 +208,7 @@ create_storm_gif_tasks <- function(timestep_ind, storm_track_cfg, folders){
       )
     }
   )
-
+  
   gif_frame <- scipiper::create_task_step(
     step_name = 'gif_frame',
     target_name = function(task_name, step_name, ...){
@@ -260,7 +261,7 @@ create_storm_gif_tasks <- function(timestep_ind, storm_track_cfg, folders){
         sep="\n      ")
     }
   )
-
+  
   step_list <- list(
     sites_frame, if(has_storm_track) storm_point_frame, precip_frame,
     spark_frame, datetime_frame, legend_frame, gif_frame, gif_test_frame)
--- 6_visualize/src/prep_major_cities_fun.R
+++ 6_visualize/src/prep_major_cities_fun.R
@@ -1,12 +1,12 @@
 prep_major_cities_fun <- function(cities_ind){
   cities <- readRDS(sc_retrieve(cities_ind))
-
+  
   if(nrow(cities) > 0) {
     # check for extreme text_angles
     if(any(cities$text_angle < -360 | cities$text_angle > 360)) {
       stop("text_angle must be between -360 and 360")
     }
-
+    
     # compute label coordinates and orientation
     cities <- cities %>%
       mutate(
@@ -23,11 +23,11 @@ prep_major_cities_fun <- function(cities_ind){
           text_angle > 45 & text_angle <= 135 ~ 3,
           text_angle > 315 | text_angle <= 45 ~ 4))
   }
-
+  
   plot_fun <- function(){
-
+    
     if(nrow(cities) == 0) return()
-
+    
     # compute text coords from angle and distance as fraction of x range of plot
     xrange <- diff(par('usr')[c(1,2)])
     cities <- cities %>%
@@ -42,7 +42,7 @@ prep_major_cities_fun <- function(cities_ind){
            bg = cities$dot_bg,
            col = cities$dot_col,
            cex = cities$dot_cex)
-
+    
     # plot the city labels
     text(x = cities$text_x,
          y = cities$text_y,
@@ -51,18 +51,13 @@ prep_major_cities_fun <- function(cities_ind){
          pos = cities$text_pos,
          col = cities$text_col,
          font = cities$text_font,
-         offset = 0.2)
+         offset = 0) # use offset = 2 if adding callout lines
     
-    for (city_name in cities$name){
-      city <- cities %>% filter(name == city_name)
-      
-      segments(city$text_x, y0 = city$text_y, x1 = city$dot_x, y1 = city$dot_y)
-    }
+    # add callout lines between points and labels
+    # segments(x0=cities$text_x, y0=cities$text_y, x1=cities$dot_x, y1=cities$dot_y, col=cities$dot_col)
     
-
   }
   return(plot_fun)
 }
-
 
 
