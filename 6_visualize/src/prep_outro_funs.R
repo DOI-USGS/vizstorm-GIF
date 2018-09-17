2b80a02251da88610e76c55b786558e2fbe02a60
Jordan S Read <jread@usgs.gov>
2018-09-17 08:30:50 -0500

    adding untracked files

---
+++ 6_visualize/src/prep_outro_funs.R
@@ -0,0 +1,109 @@
+prep_outro_rdgs_fun <- function(rdg_ind="1_fetch/out/rapid_dep_sites.rds.ind", gage_col_config, outro_placement, legend_text_cfg, opacity=1) {
+  # project from lat/lon to plot coordinates. . this properly belongs in a 2_process step, but we're releasing tomorrow
+  proj_str <- "+proj=lcc +lat_1=34.83333333333334 +lat_2=32.5 +lat_0=31.83333333333333 +lon_0=-81 +x_0=609600 +y_0=0 +ellps=GRS80 +units=m +no_defs "
+  rdgs <- readRDS(sc_retrieve(rdg_ind)) %>%
+    sf::st_as_sf(coords=c('longitude', 'latitude'), crs=4326) %>%
+    sf::st_transform(crs = proj_str) %>%
+    select(site_no, geometry)
+  
+  if(opacity != 1) stop("opacity other than 1 not yet supported")
+  
+  stn_pch <- 19
+  stn_col <- gage_col_config$gage_stn_col
+  stn_cex <- 1.3
+  font_add_google('Abel', "abel")
+  font_add_google('Oswald', "Oswald")
+  
+  plot_fun <- function(){
+    # plot the gage points
+    plot(sf::st_geometry(rdgs$geometry), add = TRUE,
+         pch = stn_pch, col = stn_col, cex = stn_cex*0.75)
+    
+    # plan text and legend coordinates
+    user_coords <- par()$usr
+    line_spacing_vert <- strheight("A", cex=legend_text_cfg$cex, family=legend_text_cfg$family)
+    dot_spacing_vert <- line_spacing_vert / 3
+    
+    x_title <- user_coords[1] + 0.5 * diff(user_coords[1:2])
+    x_text <- user_coords[1] + 0.5 * diff(user_coords[1:2])
+    y_title <- user_coords[3] + 0.88 * diff(user_coords[3:4])
+    y_text <- user_coords[3] + 0.84 * diff(user_coords[3:4])
+    x_dot <- x_text + 0.128 * diff(user_coords[1:2])
+    y_dot <- y_text - 0.0392 * diff(user_coords[3:4])
+    # plot text and legend
+  
+    text(x=x_title, y=y_title, labels="RAPID RESPONSE TO FLOODS", adj=c(0, 1),
+         cex=3, col=legend_text_cfg$col, family = 'Oswald')
+    text_chars <- "During floods, USGS rapidly deploys extra gages\n(orange dots   ) to help protect life and property."
+    text(x=x_text, y=y_text, labels=text_chars, adj=c(0, 1),
+         cex=2.2, col=legend_text_cfg$col, family = 'abel')
+    points(x=x_dot, y=y_dot, pch = stn_pch, col = stn_col, cex = stn_cex)
+  }
+  return(plot_fun)
+}
+
+prep_outro_allsites_fun <- function(allsites_ind="2_process/out/gage_sites_geom.rds.ind",
+                                    gage_col_config, outro_placement, legend_text_cfg, opacity=1) {
+  
+  # gage_sites_geom.rds.ind is already projected for us
+  allsites <- readRDS(sc_retrieve(allsites_ind))
+  
+  if(opacity != 1) stop("opacity other than 1 not yet supported")
+  
+  ltn_pch <- 19
+  ltn_col <- gage_col_config$gage_ltn_col
+  ltn_cex <- 1.3
+  
+  font_add_google('Abel', "abel")
+  font_add_google('Oswald', "Oswald")
+  
+  plot_fun <- function(){
+    
+    # plot the gage points
+    plot(sf::st_geometry(allsites$geometry), add = TRUE,
+         pch = ltn_pch, col = ltn_col, cex = ltn_cex*0.5)
+    
+    # plan text and legend coordinates
+    user_coords <- par()$usr
+    x_title <- user_coords[1] + 0.215 * diff(user_coords[1:2])
+    x_text <- user_coords[1] + 0.215 * diff(user_coords[1:2])
+    y_title <- user_coords[3] + 0.45 * diff(user_coords[3:4])
+    y_text <- user_coords[3] + 0.41 * diff(user_coords[3:4])
+    x_dot <- x_text + 0.678 * diff(user_coords[1:2])
+    y_dot <- y_text - 0.01 * diff(user_coords[3:4])
+    
+    # plot text and legend
+    text(x=x_title, y=y_title, labels="NATIONAL SCALE OBSERVING NETWORK", adj=c(0, 1),
+         cex=3, col=legend_text_cfg$col, family = 'Oswald')
+    text_chars <- "USGS monitors water levels at thousands of gages nationally (blue dots   ). 
+Long-term USGS gages across the southeastern US are shown on the map."
+    text(x=x_text, y=y_text, labels=text_chars, adj=c(0, 1),
+         cex=2.2, col=legend_text_cfg$col, family = 'abel')
+    
+    points(x=x_dot, y=y_dot, pch = ltn_pch, col = ltn_col, cex = ltn_cex)
+  }
+  return(plot_fun)
+}
+
+prep_outro_readmore_fun <- function(outro_placement, legend_text_cfg, opacity=1) {
+  
+  if(opacity != 1) stop("opacity other than 1 not yet supported")
+  
+  plot_fun <- function(){
+    
+    # plan text coordinates
+    user_coords <- par()$usr
+    x_title <- user_coords[1] + 0.54 * diff(user_coords[1:2])
+    x_text <- user_coords[1] + 0.48 * diff(user_coords[1:2])
+    y_title <- user_coords[3] + 0.63 * diff(user_coords[3:4])
+    y_text <- user_coords[3] + 0.59 * diff(user_coords[3:4])
+    
+    # plot text and legend
+    text(x=x_title, y=y_title, labels="STAY SAFE DURING FLOODS", adj=c(0, 1),
+         cex=3, col=legend_text_cfg$col, family = 'Oswald')
+    text_chars <- "Learn more: www.usgs.gov/florence"
+    text(x=x_text, y=y_text, labels=text_chars, adj=c(0, 1),
+         cex=2.2, col=legend_text_cfg$col, family = 'abel')
+  }
+  return(plot_fun)
+}
