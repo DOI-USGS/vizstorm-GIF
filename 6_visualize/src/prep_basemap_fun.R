257d83fc8eb5a94c39dc5f7f9b7d8fde0d2887b6
Alison Appling <aappling@usgs.gov>
2018-10-12 14:23:08 -0400

    improve great lakes / canada

--- 6_visualize/src/prep_basemap_fun.R
+++ 6_visualize/src/prep_basemap_fun.R
@@ -3,7 +3,7 @@ prep_basemap_fun <- function(focus_geoms_ind, secondary_geoms_ind = NULL, detail
   plot_fun <- function(){
     if (!is.null(secondary_geoms_ind)){
       secondary_geoms <- readRDS(sc_retrieve(secondary_geoms_ind))
-      plot(secondary_geoms, add = TRUE, lwd = 0.3, col = 'grey80', border = 'grey75') # should style args be in a config?
+      plot(secondary_geoms, add = TRUE, col = 'grey95', border = 'grey75') # should style args be in a config?
     }
 
     if (!is.null(detail_geoms_ind)){
