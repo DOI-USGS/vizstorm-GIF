7be9a83c8d884f08569e20e8d498cf9606f09ba1
Platt <lplatt@usgs.gov>
2019-08-26 15:29:37 -0500

    use scipiper default combiner methods for intro, outro, and storm makefiles

--- 6_visualize/src/create_gif_makefile.R
+++ 6_visualize/src/create_gif_makefile.R
@@ -7,8 +7,7 @@ create_intro_gif_makefile <- function(makefile, task_plan, remake_file) {
       '6_visualize/src/prep_spark_line_fun.R',
       '6_visualize/src/prep_gage2spark_fun.R',
       '6_visualize/src/create_animation_frame.R'),
-    file_extensions=c('feather','ind'),
-    ind_complete=TRUE)
+    file_extensions=c('feather','ind'))
 }
 
 create_storm_gif_makefile <- function(makefile, task_plan, remake_file) {
@@ -23,6 +22,5 @@ create_storm_gif_makefile <- function(makefile, task_plan, remake_file) {
       '6_visualize/src/prep_storm_point_fun.R',
       '6_visualize/src/prep_precip_fun.R',
       '6_visualize/src/prep_spark_line_fun.R'),
-    file_extensions=c('feather','ind'),
-    ind_complete=TRUE)
+    file_extensions=c('feather','ind'))
 }
