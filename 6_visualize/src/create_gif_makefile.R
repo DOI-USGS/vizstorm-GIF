632a1f70b34f6f0da4eb52a47a20c9d0f5358f09
Platt <lplatt@usgs.gov>
2019-08-26 12:45:12 -0500

    fix weirdness with resulting makefile

--- 6_visualize/src/create_gif_makefile.R
+++ 6_visualize/src/create_gif_makefile.R
@@ -25,7 +25,7 @@ create_storm_gif_makefile <- function(makefile, task_plan, remake_file, final_ta
       '6_visualize/src/prep_spark_line_fun.R',
       '6_visualize/src/combine_animation_frames.R'),
     file_extensions=c('feather','ind'),
-    final_targets,
+    final_targets = final_targets,
     finalize_funs = "combine_animation_frames"
     )
 }
