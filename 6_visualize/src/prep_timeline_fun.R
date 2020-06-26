a1459ad2c79e7000a691b158f857d93500ef7e52
Platt <lplatt@usgs.gov>
2020-06-26 11:29:41 -0400

    rest of timeline formatting

--- 6_visualize/src/prep_timeline_fun.R
+++ 6_visualize/src/prep_timeline_fun.R
@@ -6,10 +6,6 @@ prep_timeline_fun <- function(DateTime, timestep_ind, timeline_config, spark_con
   storm_timesteps <- fetch_read(timestep_ind)
   date_lims <- as.POSIXct(range(storm_timesteps), tz = "UTC")
 
-  # Compute normalized plot coordinates for all sites
-  x_coords <- c(spark_config$xleft, spark_config$xright)
-  y_coords <- spark_config$ytimeline
-
   # Define functions that will convert each site's shapes to user coordinates
   dateTime_to_x <- function(dateTime, x_user) {
     date_lims_num <- as.numeric(date_lims, units='days')
@@ -22,24 +18,23 @@ prep_timeline_fun <- function(DateTime, timestep_ind, timeline_config, spark_con
   # when making the full plot
   plot_fun <- function(){
 
-    # closure needs x_coords, y_coords, shapes, dateTime_to_x(), stage_to_y()
-
     # Ask the open device for the user coordinates
     coord_space <- par()$usr
 
     # Convert normalized plot coordinates to user coordinates
-    loc_x <- coord_space[1] + x_coords * diff(coord_space[1:2])
-    loc_y <- coord_space[3] + y_coords * diff(coord_space[3:4])
-    loc_date_y <- loc_y - loc_y*0.05
+    loc_x <- coord_space[1] + c(spark_config$xleft, spark_config$xright) * diff(coord_space[1:2])
+    loc_y <- coord_space[3] + spark_config$ytimeline * diff(coord_space[3:4])
+    loc_date_y <- coord_space[3] + spark_config$ytimeline_text * diff(coord_space[3:4])
 
     # Convert this the current date to user coordinates
     loc_date_x <- dateTime_to_x(this_DateTime, loc_x)
 
     # Add timeline and dot to plot
-    points(loc_x, rep(loc_y, length(x_coords)), type = 'l', col = timeline_config$line_col)
+    points(loc_x, rep(loc_y, length(loc_x)), type = 'l', col = timeline_config$line_col,
+           lwd = timeline_config$line_lwd, lty = timeline_config$line_lty)
     points(loc_date_x, loc_y, col = timeline_config$pt_col, cex=timeline_config$pt_cex, pch = timeline_config$pt_pch)
-    text(x=loc_date_x, y=loc_date_y, labels=format(this_DateTime, "%B %d"), adj=c(0.5, 0),
-         family=legend_text_cfg$family, cex = timeline_config$line_cex)
+    text(x=loc_date_x, y=loc_date_y, labels=format(this_DateTime, "%b %d"), adj=c(0.5, 0.5),
+         family=legend_text_cfg$family, cex = timeline_config$font_cex, col = timeline_config$font_col)
 
   }
 
