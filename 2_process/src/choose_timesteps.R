4926584d5c67e3c2f5e3c040d25f4c63e96ebe45
Jordan S Read <jread@usgs.gov>
2020-09-22 07:58:00 -0500

    updated with difference gages

--- 2_process/src/choose_timesteps.R
+++ 2_process/src/choose_timesteps.R
@@ -1,5 +1,5 @@
 choose_timesteps <- function(ind_file, dates) {
-  timesteps <- head(seq(as.POSIXct(dates$start, tz = "UTC"), as.POSIXct(dates$end, tz = "UTC"), by = 'hours'), -4L)
+  timesteps <- head(seq(as.POSIXct(dates$start, tz = "UTC"), as.POSIXct(dates$end, tz = "UTC"), by = 'hours'), -6L)
   data_file <- as_data_file(ind_file)
   saveRDS(timesteps, data_file)
   gd_put(ind_file, data_file)
--- 2_process/src/filter_sites_custom.R
+++ 2_process/src/filter_sites_custom.R
@@ -43,7 +43,10 @@ filter_sites_custom <- function(ind_file, sites_ind, dates) {
   # sites_info_subset <- sites_info_to_choose_from %>%
   #   sample_n(ifelse(nrow(sites_info_to_choose_from) >= 6, 6, nrow(sites_info_to_choose_from)))
 
-  sites_info_subset <- filter(sites_df, site_no %in% c('02378780','02378790','02362000','02374250', '02363000', '02370000', '02365470', '02359000', '02329600'))
+  sites_info_subset <- filter(sites_df, site_no %in%
+                                c('02378780','02378790','02362000','02374250', '02363000', '02370000',
+                                  '02365470', '02359000', '02329600', '02327100', '02365200', '02363000', '02374250',
+                                  '02351890'))
 
   # write the data file and the indicator file
   if(packageVersion('scipiper') < package_version('0.0.11')) stop('1-arg version of gd_put requires scipiper 0.0.11+')
