5fa5826ff1f2c46c6a16cf599e49341ba960b9b2
collnell <cnell@usgs.gov>
2021-08-28 13:08:03 -0500

    sp stencil bug fix for crs

--- 1_fetch/src/fetch_precip_nc.R
+++ 1_fetch/src/fetch_precip_nc.R
@@ -13,17 +13,17 @@ fetch_precip_data <- function(ind_file, view_polygon, times) {
   # create the stencil from the bbox of the view_polygon
   crs <- st_crs(view_polygon)
   bbox <- st_bbox(view_polygon)
-  
+
   precip_boundary <- "POLYGON ((-80.74672 19.80368, -119.023 23.117, -134.043 53.51192, -124.9736 55.90588, -114.5051 57.4236, -102.7265 57.82357, -91.28471 56.95466, -81.85043 55.19577, -73.63054 52.69872, -66.4331 49.53428, -59.95137 45.6187, -80.74672 19.80368))" %>%
     st_as_sfc(crs = st_crs("+proj=longlat +datum=WGS84"))
-  
+
   stencil <- st_transform(view_polygon, "+proj=longlat +datum=WGS84") %>%
     st_intersection(precip_boundary)
-  
+
   stencil <- as_Spatial(stencil)
-  stencil@proj4string <- sp::CRS("+proj=longlat +datum=WGS84") # geoknife complains otherwise
+  sp::proj4string(stencil)  <- sp::CRS("+proj=longlat +datum=WGS84") # geoknife complains otherwise
   stencil <- geoknife::simplegeom(stencil)
-  
+
   # run the job and retrieve the results
   job <- geoknife(stencil = stencil, fabric = fabric, knife = knife)
   nc_file <- as_data_file(ind_file)
@@ -42,11 +42,11 @@ fetch_precip_data <- function(ind_file, view_polygon, times) {
 # x <- matrix(rep(c(1:ncol(lon)), nrow(lon)),
 #             nrow = nrow(lon), ncol = ncol(lon),
 #             byrow = TRUE)
-# 
+#
 # y <- matrix(rep(c(1:nrow(lon)), ncol(lon)),
 #             nrow = nrow(lon), ncol = ncol(lon),
 #             byrow = FALSE)
-# 
+#
 # sf_points <- data.frame(x = matrix(x, ncol = 1),
 #                         y = matrix(y, ncol = 1),
 #                         lon = matrix(lon, ncol = 1),
--- 1_fetch/src/fetch_river_geometry.R
+++ 1_fetch/src/fetch_river_geometry.R
@@ -1,34 +1,13 @@
 fetch_major_river_geoms <- function(ind_file, view_polygon, fetch_streamorder) {
 
-  bbox <- st_bbox(st_transform(view_polygon, 4326))
+  bbox_sf <- st_as_sfc(st_bbox(view_polygon))
 
-  postURL <- "https://cida.usgs.gov/nwc/geoserver/nhdplus/ows"
-
-  filterXML <- paste0('<?xml version="1.0"?>',
-                      '<wfs:GetFeature xmlns:wfs="http://www.opengis.net/wfs" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:gml="http://www.opengis.net/gml" service="WFS" version="1.1.0" outputFormat="application/json" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd">',
-                      '<wfs:Query xmlns:feature="http://gov.usgs.cida/nhdplus" typeName="feature:nhdflowline_network" srsName="EPSG:4326">',
-                      '<ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">',
-                      '<ogc:And>',
-                      '<ogc:PropertyIsGreaterThan>',
-                      '<ogc:PropertyName>streamorde</ogc:PropertyName>',
-                      '<ogc:Literal>',fetch_streamorder,'</ogc:Literal>',
-                      '</ogc:PropertyIsGreaterThan>',
-                      '<ogc:BBOX>',
-                      '<ogc:PropertyName>the_geom</ogc:PropertyName>',
-                      '<gml:Envelope>',
-                      '<gml:lowerCorner>',bbox[2]," ",bbox[1],'</gml:lowerCorner>',
-                      '<gml:upperCorner>',bbox[4]," ",bbox[3],'</gml:upperCorner>',
-                      '</gml:Envelope>',
-                      '</ogc:BBOX>',
-                      '</ogc:And>',
-                      '</ogc:Filter>',
-                      '</wfs:Query>',
-                      '</wfs:GetFeature>')
-
-  out <- httr::POST(postURL, body = filterXML)
-
-  sf_major_rivers <- read_sf(rawToChar(out$content)) %>%
-    st_transform(st_crs(view_polygon))
+  sf_major_rivers <- get_nhdplus(AOI = bbox_sf,
+                                 realization = "flowline",
+                                 streamorder = fetch_streamorder$fetch_streamorder) %>%
+    group_by(gnis_id, streamorde) %>%
+    summarize() %>%
+    ms_simplify()
 
   data_file <- as_data_file(ind_file)
   saveRDS(sf_major_rivers, data_file)
@@ -37,35 +16,12 @@ fetch_major_river_geoms <- function(ind_file, view_polygon, fetch_streamorder) {
 
 fetch_waterbody_geoms <- function(ind_file, view_polygon, fetch_waterbody_areasqkm) {
 
-  bbox <- st_bbox(st_transform(view_polygon, 4326))
+  bbox_sf <- st_as_sfc(st_bbox(view_polygon))
 
-  postURL <- "https://cida.usgs.gov/nwc/geoserver/nhdplus/ows"
-
-  filterXML <- paste0('<?xml version="1.0"?>',
-                      '<wfs:GetFeature xmlns:wfs="http://www.opengis.net/wfs" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:gml="http://www.opengis.net/gml" service="WFS" version="1.1.0" outputFormat="application/json" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd">',
-                      '<wfs:Query xmlns:feature="http://gov.usgs.cida/nhdplus" typeName="feature:nhdwaterbody" srsName="EPSG:4326">',
-                      '<ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">',
-                      '<ogc:And>',
-                      '<ogc:PropertyIsGreaterThan>',
-                      '<ogc:PropertyName>areasqkm</ogc:PropertyName>',
-                      '<ogc:Literal>',fetch_waterbody_areasqkm,'</ogc:Literal>',
-                      '</ogc:PropertyIsGreaterThan>',
-                      '<ogc:BBOX>',
-                      '<ogc:PropertyName>the_geom</ogc:PropertyName>',
-                      '<gml:Envelope>',
-                      '<gml:lowerCorner>',bbox[2]," ",bbox[1],'</gml:lowerCorner>',
-                      '<gml:upperCorner>',bbox[4]," ",bbox[3],'</gml:upperCorner>',
-                      '</gml:Envelope>',
-                      '</ogc:BBOX>',
-                      '</ogc:And>',
-                      '</ogc:Filter>',
-                      '</wfs:Query>',
-                      '</wfs:GetFeature>')
-
-  out <- httr::POST(postURL, body = filterXML)
-
-  sf_waterbodies <- read_sf(rawToChar(out$content)) %>%
-    st_transform(st_crs(view_polygon))
+  sf_waterbodies <- get_waterbodies(AOI = bbox_sf,
+                                    buffer = 0) %>%
+    filter(areasqkm > fetch_waterbody_areasqkm$fetch_waterbody_areasqkm) %>%
+    ms_simplify()
 
   data_file <- as_data_file(ind_file)
   saveRDS(sf_waterbodies, data_file)
